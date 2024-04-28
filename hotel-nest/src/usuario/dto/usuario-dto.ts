6c6f92a2008a3241aa5f6a92e8ee99218031ea2d
Muyiam <liam.alejoasensi@gmail.com>
2024-04-29 00:08:11 +0200

    Añadiendo validación a los usuarios

--- hotel-nest/src/usuario/dto/usuario-dto.ts
+++ hotel-nest/src/usuario/dto/usuario-dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class UsuarioDto {
+  @IsString({ message: 'El login debe ser un texto' })
+  @IsNotEmpty({ message: 'El login es requerido' })
   readonly login: string;
+
+  @IsString({ message: 'El password debe ser un texto' })
+  @IsNotEmpty({ message: 'El password es requerido' })
   readonly password: string;
 }
